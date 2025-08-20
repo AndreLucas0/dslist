112988edff733e053f7b4806868c096d93aec899
Andre Lucas Ferreira <alucas3103@outlook.com>
2025-08-19 22:51:42 -0300

    remove all unused imports

--- src/main/java/com/devsuperior/dslist/controller/GameController.java
+++ src/main/java/com/devsuperior/dslist/controller/GameController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslist.dto.GameMiniDto;
-import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.service.GameService;
 
 @RestController
