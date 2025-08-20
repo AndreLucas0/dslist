4af7a152c8a61a205de4fe0deca5075222ee1551
Andre Lucas Ferreira <alucas3103@outlook.com>
2025-08-20 17:47:14 -0300

    faltou os imports sql

---
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+    
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+
+    private Integer position;
+
+    public Belonging(Game game, GameList gameList, Integer position) {
+        id.setGame(game);
+        id.setGameList(gameList);
+        this.position = position;
+    }
+
+    
+}
